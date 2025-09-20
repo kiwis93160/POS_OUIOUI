7f6b395c42e0d20c17779a147a9d55bc6e95c005
kiwis93160 <jeremie.bercet@gmail.com>
2025-09-20 16:30:44 +0200

    Set up Tailwind CSS build pipeline

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss';
+
+export default {
+  content: [
+    './index.html',
+    './index.tsx',
+    './App.tsx',
+    './types.ts',
+    './components/**/*.{ts,tsx,js,jsx}',
+    './hooks/**/*.{ts,tsx,js,jsx}',
+    './pages/**/*.{ts,tsx,js,jsx}',
+    './services/**/*.{ts,tsx,js,jsx}',
+    './src/**/*.{ts,tsx,js,jsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
